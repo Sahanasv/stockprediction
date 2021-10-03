d9bdb2ed63ee3b59a67584fdc51afbcf52e7779b
Sahanasv <88230021+Sahanasv@users.noreply.github.com>
2021-10-03 23:32:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, render_template,request,session
+
+from sklearn.metrics import accuracy_score
+
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+
+@app.route('/prediction1',methods=['GET','POST'])
+def pred():
+    a=[]
+    if request.method=="POST":
+        open = request.form['op']
+        low = request.form['low']
+        high = request.form['high']
+        vol = request.form['vol']
+        a.extend([open,low,high,vol])
+        model=joblib.load('banks.pkl')
+        y_pred=model.predict([a])
+        return render_template('prediction.html',msg="success",op=y_pred)
+    return render_template('prediction.html')
+
+
+if __name__ == '__main__':
+    app.secret_key="sahana"
+    app.run(debug=True)
